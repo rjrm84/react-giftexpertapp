481cb0aa748c85ccb21a5992d9ab13c68c31c8b1
xxulverxx <rjrm84@gmail.com>
2021-05-23 21:19:26 -0300

    añadiendo docs al repo

---
+++ docs/precache-manifest.7cf8a9bf573783113cf04ea38445b974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5679b5b092aace2184d81b697d0abd33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5493026531a580be8f64",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5493026531a580be8f64",
+    "url": "/static/js/main.33435cad.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
